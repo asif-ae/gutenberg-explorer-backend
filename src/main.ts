5755018dd94862b12de885d1456a187074cdb064
Asif Estiak <me.asif.ae@gmail.com>
2024-11-03 19:53:04 +0600

    fix: updated cors

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://gutenberg-explorer-frontend.vercel.app',
-    ], // Replace with your frontend URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    origin: '*', // Allow all origins (use with caution in production)
+    // origin: [
+    //   'http://localhost:3000',
+    //   'https://gutenberg-explorer-frontend.vercel.app',
+    // ], // Replace with your frontend URL
+    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    // credentials: true,
   });
 
   await app.listen(process.env.PORT ?? 3001);
