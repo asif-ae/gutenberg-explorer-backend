798aff1008a4a0c357920e6744ed1d79870ac423
Asif Estiak <me.asif.ae@gmail.com>
2024-11-03 21:03:05 +0600

    fix: updated new cors policy

--- src/main.ts
+++ src/main.ts
@@ -5,36 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        'https://gutenberg-explorer-frontend.vercel.app',
-        'http://localhost:3000',
-      ];
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: [
+      'https://gutenberg-explorer-frontend.vercel.app',
+      'http://localhost:3000',
+    ],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-  });
-
-  // Handle preflight OPTIONS request
-  app.use((req, res, next) => {
-    if (req.method === 'OPTIONS') {
-      res.header(
-        'Access-Control-Allow-Methods',
-        'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
-      );
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Authorization'
-      );
-      return res.sendStatus(204); // No Content
-    }
-    next();
   });
 
   await app.listen(process.env.PORT ?? 3001);
